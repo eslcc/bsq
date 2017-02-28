59554919b714d3e4a0901ca1170672f4ab194dd9
Marks Polakovs <marks@markspolakovs.me>
2017-02-28 18:01:07 +0100

    Update protobuf compile script to automatically fix py3 relative imports

--- compile_proto_python.cmd
+++ compile_proto_python.cmd
@@ -1,5 +1,5 @@
 @echo off
 protoc -I=proto/models --python_out=server/app/modules/models proto/models/*.proto
 robocopy proto server\app\proto /E
-echo Note: Remember to edit the generated source to change to Python 3 relative imports
-echo (change `from models import ...` to `from . import ...`)
+powershell -File fix-imports.ps1
+echo Done
