9b23777d4f69d9f0fab541e8ce0c43a26d3438db
Marks Polakovs <marks@markspolakovs.me>
2017-03-11 19:45:38 +0100

    Un-revert handler

--- server/src/main/java/club/eslcc/bigsciencequiz/server/handlers/IdentifyUserHandler.java
+++ server/src/main/java/club/eslcc/bigsciencequiz/server/handlers/IdentifyUserHandler.java
@@ -2,15 +2,23 @@ package club.eslcc.bigsciencequiz.server.handlers;
 
 import static club.eslcc.bigsciencequiz.proto.Rpc.*;
 
+import club.eslcc.bigsciencequiz.proto.User;
 import club.eslcc.bigsciencequiz.server.IRpcHandler;
+import club.eslcc.bigsciencequiz.server.Redis;
 import club.eslcc.bigsciencequiz.server.RedisHelpers;
 import club.eslcc.bigsciencequiz.server.SocketHandler;
 import org.eclipse.jetty.websocket.api.Session;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by marks on 11/03/2017.
  */
 public class IdentifyUserHandler implements IRpcHandler {
+    private static Jedis jedis = Redis.getJedis();
+
     @Override
     public RpcResponse handle(String currentUserId, RpcRequest request, Session session) {
         RpcResponse.Builder builder = RpcResponse.newBuilder();
@@ -18,10 +26,25 @@ public class IdentifyUserHandler implements IRpcHandler {
 
         IdentifyUserRequest idR = request.getIdentifyUserRequest();
 
-//        SocketHandler.users.put(session, idR.getDeviceId());
+        List<String> teams = jedis.lrange("teams", 0, -1);
+        Optional<String> firstUnassigned = teams.stream().filter((team) -> jedis.hget("devices", team) == null).findFirst();
 
-        responseBuilder.setState(RedisHelpers.getGameState(idR.getDeviceId()));
+        if (firstUnassigned.isPresent()) {
+            List<String> members = jedis.lrange("team_members_" + firstUnassigned.get(), 0, -1);
 
+            jedis.hset("devices", firstUnassigned.get(), idR.getDeviceId());
+//            SocketHandler.users.put(session, idR.getDeviceId());
+
+            User.Team.Builder teamBuilder = User.Team.newBuilder();
+            teamBuilder.setNumber(firstUnassigned.get());
+            teamBuilder.addAllMemberNames(members);
+
+            responseBuilder.setTeam(teamBuilder);
+            responseBuilder.setState(RedisHelpers.getGameState(idR.getDeviceId()));
+        } else {
+            responseBuilder.setFailureReason(IdentifyUserResponse.FailureReason.NO_FREE_TEAMS);
+        }
+        builder.setIdentifyUserResponse(responseBuilder);
         return builder.build();
     }
 }
