3286f23ffc30ae76310e428114ee75ddc7830bc4
Marks Polakovs <marks@markspolakovs.me>
2017-03-11 10:23:42 +0100

    Dockerise server

--- server/src/main/java/club/eslcc/bigsciencequiz/server/Redis.java
+++ server/src/main/java/club/eslcc/bigsciencequiz/server/Redis.java
@@ -3,6 +3,8 @@ package club.eslcc.bigsciencequiz.server;
 import redis.clients.jedis.Jedis;
 
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by marks on 10/03/2017.
@@ -10,7 +12,9 @@ import java.util.Optional;
 public class Redis {
     private static Jedis jedis;
     static {
-        jedis = new Jedis(Optional.ofNullable(System.getenv("REDIS_HOST")).orElse("localhost"), 6379);
+        String host = Optional.ofNullable(System.getenv("REDIS_HOST")).orElse("localhost");
+        Logger.getGlobal().log(Level.INFO, "Connecting to Redis " + host);
+        jedis = new Jedis(host, 6379);
     }
 
     public static Jedis getJedis() {
